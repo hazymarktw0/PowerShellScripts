7b20e8aed825f7753ac821fe0180362560871038
hazymarktw0 <hazymkii@gmail.com>
2025-04-02 19:47:46 -0500

    Add files via upload
    My very first powershell script, as part of Stackroute's Advanced Cybersecurity course

---
+++ powershell test.ps1
@@ -0,0 +1,13 @@
+﻿# create folder named "ENC"
+$folderpath = "C:\Users\HazyMkII\Documents\Cyber Security\command line"
+New-Item -ItemType directory -Path $folderpath
+write-output "$folderpath was created successfully"
+# create a test document 
+$FileName ="testbed.txt"
+$testfilepath = "$folderpath\$filename"
+New-Item -ItemType file -path $testfilepath -force
+write-output "$FileName was created successfully"
+# Prompt the user for what content to add to the file we are creating 
+$content = read-host "Enter the content you would like to add to the document" 
+set-content $testfilepath -Value $content
+Write-Output "Content has been added to the document"
